e649ed9a4a0d7bcc400a475308f477bc0c509b8d
Thiago <thaguilardev@gmail.com>
2025-08-15 16:16:53 -0300

    feat: Cria os styles das msgs de error e dos hover/focus

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,22 +1,33 @@
+import ErrorMsg from "./ErrorMsg";
 import "./InputBox.css";
 
 export default function InputBox() {
+  // VARIAVEIS QUE VAO ATENTAR PARA O TIPO DE ERROR DO FORM
+  // CASO ALGUM FORM TENHA ERROR A FUNÇÃO VAI TER QUE ADD
+  // A CLASS "has-error" NO INPUT-BOX CORRESPONDENTE E ALTERAR
+  // A VARIAVEL ABAIXO
+  let dayValidate = "";
+  let monthValidate = "";
+  let yearValidate = "";
   return (
     // O <form> deve conter todo o formulário, substituir uma <div> por ele
     <form className="form-boxes">
       <div className="input-box">
         <label htmlFor="iDay">DAY</label>
         <input type="number" name="day" id="iDay" placeholder="DD" />
+        <ErrorMsg errorType={dayValidate} />
       </div>
 
       <div className="input-box">
         <label htmlFor="iMonth">MONTH</label>
         <input type="number" name="month" id="iMonth" placeholder="MM" />
+        <ErrorMsg errorType={monthValidate} />
       </div>
 
       <div className="input-box">
         <label htmlFor="iYear">YEAR</label>
         <input type="number" name="year" id="iYear" placeholder="YYYY" />
+        <ErrorMsg errorType={yearValidate} />
       </div>
     </form>
   );
