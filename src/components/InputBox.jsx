3c0806ac17ab16294b59216db0ed9034a31be5ae
Raul Ferreira da Cruz Neto <raulfcruznt@gmail.com>
2025-08-16 10:30:04 -0300

    Adição da função para adcionar a classe 'has-error' no arquivo InputBox

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,15 +1,20 @@
 import ErrorMsg from "./ErrorMsg";
 import "./InputBox.css";
 
-export default function InputBox({ birthDate, setBirthDate }) {
+//ALTERAÇÃO RAUL: Acréscimo da prop "errors"
+export default function InputBox({ birthDate, setBirthDate, errors }) {
   // VARIAVEIS QUE VAO ATENTAR PARA O TIPO DE ERROR DO FORM
   // CASO ALGUM FORM TENHA ERROR A FUNÇÃO VAI TER QUE ADD
   // A CLASS "has-error" NO INPUT-BOX CORRESPONDENTE E ALTERAR
   // A VARIAVEL ABAIXO
-  let dayValidate = "";
-  let monthValidate = "";
-  let yearValidate = "";
+  // let dayValidate = "";
+  // let monthValidate = "";
+  // let yearValidate = "";
 
+  //ALTERÇÃO RAUL: Função para adicionar a classe "has-error"
+  const getErrorClass = (fieldName) => {
+    return errors[fieldName] ? "has-error" : ""; //Operador ternário, costumo usar bastante e não é tão comum...
+  }
   // Função para atualizar estado ao digitar
   const handleChange = (e) => {
     const { name, value } = e.target;
