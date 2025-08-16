1ec7595b83eea1135c736fcbf177e17396e289ae
Raul Ferreira da Cruz Neto <raulfcruznt@gmail.com>
2025-08-16 11:43:29 -0300

    Retirei o <form> que havia colocado no app.jsx por incompatibilidade

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,12 @@ import useAgeScript from "./components/useAgeScript";
 function App() {
   const { birthDate, setBirthDate, ageResult, calculateAge, errors } = useAgeScript();
 
-  // Esta função será chamada quando o formulário for submetido
-  const handleSubmit = (event) => {
-    event.preventDefault(); // Impede que a página recarregue
-    calculateAge(); //Chama a função que agora também valida os dados
-  }
-
   return (
-    <main className="container">
-      {/* O <form> envolve os inputs e o botão  ara que o submit funcione*/}
-      <form onSubmit={handleSubmit}> {/*Permite que o  enter funcione igualmente a clicar o botão*/}
-        <InputBox birthDate={birthDate} setBirthDate={setBirthDate} errors={errors} />
-        <MiddleDivider />
-      </form>
+    <div className="container">
+      <InputBox birthDate={birthDate} setBirthDate={setBirthDate} errors={errors} />
+      <MiddleDivider onAction={calculateAge} />
       <AgeDisplay ageResult={ageResult} />
-    </main>
+    </div>
   );
 }
 
